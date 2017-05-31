3c80d4d2eea0072181ef57b77074ae4c6e66750d
danialbleile <danialbleile@wsu.edu>
2017-05-31 15:56:23 -0700

    Fixed CSS path again | Bumped version 2.3.9

--- cahnrs-pagebuilder-plugin.php
+++ cahnrs-pagebuilder-plugin.php
@@ -5,7 +5,7 @@ Plugin URI: http://cahnrs.wsu.edu/communications
 Description: Builds customizable page layouts
 Author: cahnrscommunications, Danial Bleile
 Author URI: http://cahnrs.wsu.edu/communications
-Version: 2.3.8
+Version: 2.3.9
 */
 
 class CAHNRS_Pagebuilder_Plugin {
@@ -147,7 +147,7 @@ class CAHNRS_Pagebuilder_Plugin {
 		
 		$params = $this->items->get_name_versions();
 		
-		wp_enqueue_style( 'admin_css', get_bloginfo('url') . '/?cpb-get-template=stylesheet&cpb-stylesheet=admin&codes=' . implode( '_' , $params ) , false , '0.0.3' );
+		wp_enqueue_style( 'admin_css', get_site_url() . '/?cpb-get-template=stylesheet&cpb-stylesheet=admin&codes=' . implode( '_' , $params ) , false , '0.0.3' );
 		
 		wp_enqueue_script( 'admin_js', plugin_dir_url( __FILE__ ) . 'js/admin.js' , array('jquery-ui-draggable','jquery-ui-droppable','jquery-ui-sortable') , '0.0.3' , true );
 		
