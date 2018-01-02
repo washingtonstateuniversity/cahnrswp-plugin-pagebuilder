4f02a47175cbdccb040d39f2bb3a8e1ab65cecc7
danialbleile <danialbleile@wsu.edu>
2018-01-02 14:31:36 -0800

    Bump version 2.4.4

--- cahnrs-pagebuilder-plugin.php
+++ cahnrs-pagebuilder-plugin.php
@@ -5,7 +5,7 @@ Plugin URI: http://cahnrs.wsu.edu/communications
 Description: Builds customizable page layouts
 Author: cahnrscommunications, Danial Bleile
 Author URI: http://cahnrs.wsu.edu/communications
-Version: 2.4.3
+Version: 2.4.4
 */
 
 class CAHNRS_Pagebuilder_Plugin {
